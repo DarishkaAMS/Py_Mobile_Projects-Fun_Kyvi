f0bb9b80a8cf7f6a34c271b8de2e3889dde80521
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-08-03 06:45:56 +0300

    Rebuild MyFirstApp

--- Kyvi_app.py
+++ Kyvi_app.py
@@ -1,34 +1,12 @@
 import kivy
 from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
 
 
-class ChildApp(GridLayout):
-
-    def __init__(self, **kwargs):
-        super(ChildApp, self).__init__()
-        self.cols = 5
-
-        self.add_widget(Label(text='User Name'))
-        self.user_name = TextInput()
-        self.add_widget(self.user_name)
-
-        self.add_widget(Label(text='User Score'))
-        self.user_score = TextInput()
-        self.add_widget(self.user_score)
-
-        self.add_widget(Label(text='User Gender'))
-        self.user_gender = TextInput()
-        self.add_widget(self.user_gender)
-
-
-class ParentApp(App):
-
+class MyFirstApp(App):
     def build(self):
-        return ChildApp()
+        return Label(text="DarishkaAMS")
+
 
 if __name__ == "__main__":
-    ParentApp().run()
+    MyFirstApp().run()
