60e70826ad65e72efcdd6092a2308e59115c9cdb
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-08-03 06:21:55 +0300

    Add ChildApp and ParentApp

---
+++ Kyvi_app.py
@@ -0,0 +1,34 @@
+import kivy
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+
+
+class ChildApp(GridLayout):
+
+    def __init__(self, **kwargs):
+        super(ChildApp, self).__init__()
+        self.cols = 2
+
+        self.add_widget(Label(text='User Name'))
+        self.user_name = TextInput()
+        self.add_widget(self.user_name)
+
+        self.add_widget(Label(text='User Score'))
+        self.user_score = TextInput()
+        self.add_widget(self.user_score)
+
+        self.add_widget(Label(text='User Gender'))
+        self.user_gender = TextInput()
+        self.add_widget(self.user_gender)
+
+
+class ParentApp(App):
+
+    def build(self):
+        return ChildApp()
+
+if __name__ == "__main__":
+    ParentApp().run()
