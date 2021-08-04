71577c9d3a979e0f0eaa196899d7cb4770b3b169
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-08-04 06:19:29 +0300

    Complete second Tryout with the separate .kv

--- kyvi_app.py
+++ kyvi_app.py
@@ -1,5 +1,6 @@
 import kivy
 from kivy.app import App
+from kivy.properties import ObjectProperty
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
@@ -8,7 +9,20 @@ from kivy.uix.widget import Widget
 
 
 class MySecondGrid(GridLayout):
-    pass
+    first_name = ObjectProperty(None)
+    last_name = ObjectProperty(None)
+    email = ObjectProperty(None)
+    dream = ObjectProperty(None)
+
+    def press_btn(self):
+        print("First name: ", self.first_name.text)
+        print("Last name: ", self.last_name.text)
+        print("Email: ", self.email.text)
+        print("Dream: ", self.dream.text)
+        self.first_name.text = ""
+        self.last_name.text = ""
+        self.email.text = ""
+        self.dream.text = ""
 
 
 class DarishkaAMSApp(App):
